d4dce62e662bc50b892444b39bfd0c203ccb30fc
mohammed adachi <mohammed.adachi@uit.ac.ma>
2025-11-05 16:25:17 +0100

    add discriminator and generator classes with forward methods for CNN-based architecture

---
+++ generator.py
@@ -0,0 +1,37 @@
+import torch
+class GeneraterBlock(torch.nn.Module):
+    def __init__(self, in_dim, out_dim, kernel, stride):
+        super().__init__()
+        self.transconv = torch.nn.ConvTranspose3d(in_dim, out_dim, kernel, stride)
+        self.batchnorm = torch.nn.BatchNorm3d(out_dim)
+    
+    def forward(self, x):
+        x = self.transconv(x)
+        x = self.batchnorm(x)
+        return torch.nn.functional.relu(x)
+class Generator(torch.nn.Module):   
+    def __init__(self):
+        super().__init__()
+        self.transconv0 = GeneraterBlock(latent_dim, 256, (4, 1, 1), (4, 1, 1))
+        self.transconv1 = GeneraterBlock(256, 128, (1, 4, 1), (1, 4, 1))
+        self.transconv2 = GeneraterBlock(128, 64, (1, 1, 4), (1, 1, 4))
+        self.transconv3 = GeneraterBlock(64, 32, (1, 1, 3), (1, 1, 1))
+        self.transconv4 = torch.nn.ModuleList([
+            GeneraterBlock(32, 16, (1, 4, 1), (1, 4, 1))
+            for _ in range(n_tracks)
+        ])
+        self.transconv5 = torch.nn.ModuleList([
+            GeneraterBlock(16, 1, (1, 1, 12), (1, 1, 12))
+            for _ in range(n_tracks)
+        ])
+
+    def forward(self, x):
+        x = x.view(-1, latent_dim, 1, 1, 1)
+        x = self.transconv0(x)
+        x = self.transconv1(x)
+        x = self.transconv2(x)
+        x = self.transconv3(x)
+        x = [transconv(x) for transconv in self.transconv4]
+        x = torch.cat([transconv(x_) for x_, transconv in zip(x, self.transconv5)], 1)
+        x = x.view(-1, n_tracks, n_measures * measure_resolution, n_pitches)
+        return x
