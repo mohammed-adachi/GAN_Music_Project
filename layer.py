07335f2f298b6bb55c83f013eb2934ea753dd2ba
mohammed adachi <mohammed.adachi@uit.ac.ma>
2025-11-05 16:21:38 +0100

    add layer pour normaliser et stabliser les odnnes

---
+++ layer.py
@@ -0,0 +1,21 @@
+import torch
+class LayerNorm(torch.nn.Module):
+
+    def __init__(self, n_f, eps=1e-5, affine=True):
+        super().__init__()
+        self.n_f = n_f
+        self.affine = affine
+        self.eps = eps
+        if self.affine:
+            self.gamma = torch.nn.Parameter(torch.Tensor(n_f).uniform_())
+            self.beta = torch.nn.Parameter(torch.zeros(n_f))
+
+    def forward(self, x):
+        shape = [-1] + [1] * (x.dim() - 1)
+        mean = x.view(x.size(0), -1).mean(1).view(*shape)
+        std = x.view(x.size(0), -1).std(1).view(*shape)
+        y = (x - mean) / (std + self.eps)
+        if self.affine:
+            shape = [1, -1] + [1] * (x.dim() - 2)
+            y = self.gamma.view(*shape) * y + self.beta.view(*shape)
+        return y
